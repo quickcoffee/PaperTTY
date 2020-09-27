bdedb33c262b4d2e9e9b605dae0f748a9b6249a5
quickcoffee <31934316+quickcoffee@users.noreply.github.com>
2020-09-27 13:56:40 +0200

    Create setup.py
    adding setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "PaperTTY",
+    version = "0.1.6",
+    author = "Jouko Strömmer",
+    author_email = "jouko.strommer@iki.fi",
+    description = ("Python module to render a TTY or VNC on e-ink"),
+    license = "CC0(/drivers: GPL 3.0)",
+    keywords = "raspberry hat eink display driver",
+    url = "https://github.com/joukos/PaperTTY",
+    packages=['Pillow'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
